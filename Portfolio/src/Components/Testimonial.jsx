985dd29ac4373292e73bfe72b9a598bba9e6318a
DakJebbin <Jebbinp@gmail.com>
2025-01-14 20:35:27 +0100

    testimonials

--- Portfolio/src/Components/Testimonial.jsx
+++ Portfolio/src/Components/Testimonial.jsx
@@ -1,3 +1,4 @@
+import { assets } from "../assets/assets"
 
 
 const Testimonial = () => {
@@ -8,23 +9,65 @@ const Testimonial = () => {
                 <h1 className="font-Inter text-white font-semibold text-[40px]">Testimonials.</h1>
 
                 <div className="flex text-white gap-5 mt-10 justify-center flex-wrap">
-                    <div className="basis-72 bg-">
+                    <div className="basis-72 bg-[#322938] shadow-lg shadow-black hover:-translate-y-2 duration-500 p-5 rounded-lg">
                         <p className="text-[30px] font-anton font-bold">&quot;</p>
-                        <p className="text-justify font-Inter">
+                        <p className=" font-Inter">
                         Jebbin is an exceptionally skilled full-stack web developer who consistently delivers high-quality projects on time. His ability to design both the front-end and back-end of applications with precision is truly impressive. He is detail-oriented, creative, and constantly pushes the boundaries of what&apos;s possible.
                         </p>
+                        <div className="flex mt-3 justify-between">
+                            <div >
+                        <p className="font-Inter">
+                            @Victor Alabo
+                        </p>
+                        <p className="font-Inter text-[#c7c7c7]">
+                            FullStack Web Developer
+                        </p>
+                        </div>
+
+                        <div className="w-10">
+                            <img className="w-full" src={assets.victor} alt="" />
+                        </div>
+                        </div>
                     </div>
-                    <div className="basis-72">
+                    <div className="basis-72 bg-[#322938] shadow-lg shadow-black hover:-translate-y-2 duration-500 p-5 rounded-lg">
                     <p className="text-[30px] font-anton font-bold">&quot;</p>
-                        <p className="text-justify font-Inter">
-                        Jebbin is a brilliant web developer with a deep understanding of full-stack technologies. His technical expertise combined with his problem-solving skills made working with him a seamless experience. He not only builds user-friendly, efficient applications but also takes the time to ensure that every project is optimized for performance.
+                        <p className="font-Inter">
+                        Jebbin is a brilliant web developer with a deep understanding of full-stack technologies. His technical expertise combined with his problem-solving skills made working with him a seamless experience.
                     </p>
+                    <div className="flex mt-3 justify-between">
+                            <div >
+                        <p className="font-Inter">
+                            @Victor Alabo
+                        </p>
+                        <p className="font-Inter text-[#c7c7c7]">
+                            FullStack Web Developer
+                        </p>
+                        </div>
+
+                        <div className="w-10">
+                            <img className="w-full" src={assets.victor} alt="" />
+                        </div>
+                        </div>
                     </div>
-                    <div className="basis-72">
+                    <div className="basis-72 bg-[#322938] shadow-lg shadow-black hover:-translate-y-2 duration-500 p-5 rounded-lg">
                     <p className="text-[30px] font-anton font-bold">&quot;</p>
-                        <p className="text-justify font-Inter">
+                        <p className=" font-Inter">
                         Jebbin is a standout full-stack developer who goes above and beyond with every project. His versatility across front-end and back-end technologies allows him to tackle complex tasks with ease. Whether working on user interfaces or handling databases and server-side logic.
                         </p>
+                        <div className="flex mt-3 justify-between">
+                            <div >
+                        <p className="font-Inter">
+                            @Victor Alabo
+                        </p>
+                        <p className="font-Inter text-[#c7c7c7]">
+                            FullStack Web Developer
+                        </p>
+                        </div>
+
+                        <div className="w-10">
+                            <img className="w-full" src={assets.victor} alt="" />
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- Portfolio/src/assets/assets.js
+++ Portfolio/src/assets/assets.js
@@ -1,10 +1,12 @@
 import logo from "./logo-removebg-preview (1).png"
 import circleImage from "./Circle_image-removebg.png"
 import innovative from "./Capture.png"
+import victor from "./cropped_image.png"
 
 export const assets = 
     {
         logo,
         circleImage,
-        innovative
+        innovative, 
+        victor
     }
